caef9f7bf737ad4268e2197976c49fc8c16eb669
Shayan Mufti <wise.mufti@gmail.com>
2022-05-28 15:58:50 -0500

    stocks app

---
+++ Shared/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  Shared
+//
+//  Created by Mufti, Shayan on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
